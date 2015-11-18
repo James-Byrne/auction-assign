c86b8c91f26f122e68821de316e0921c34560ce7
James Byrne <jamesdesbyrne@gmail.com>
2015-11-18 10:10:23 +0000

    Added input & output, Added the error reporting through output stream

--- server/ClientHandler.java
+++ server/ClientHandler.java
@@ -15,8 +15,8 @@ class ClientHandler extends Thread {
 
   private Socket client;
   private ItemHandler itemHandler;
-  private Scanner input;
-  private PrintWriter output;
+  private DataInputStream input;
+  private PrintStream output;
   private String currentBid;
 
   public ClientHandler(Socket client, ItemHandler itemHandler) {
@@ -25,9 +25,21 @@ class ClientHandler extends Thread {
     this.itemHandler = itemHandler;
   }
 
-  public void run(){
-    // String for user input
-    String userMessage;
+  public void run() {
+
+    // Create the input and output streams for communication with the user
+    try {
+      input = new DataInputStream(client.getInputStream());
+      output = new PrintStream(serviceSocket.getOutputStream());
+      // TODO : Remove below if the above works
+      // input = new Scanner(client.getInputStream());
+      // output = new PrintWriter(
+      // client.getOutputStream(), true
+      // );
+    }
+    catch(IOException ioEx) {
+      ioEx.printStackTrace();
+    }
 
     // Give the user instructions
     output.println("\nHello! Welcome to the auction\n");
@@ -41,37 +53,28 @@ class ClientHandler extends Thread {
 
     // Handle clients input & bids
     do {
-      // Accept message from the client
-      // userMessage = input.nextLine();
-
-      // Accept messages from the user
-      try {
-        input = new Scanner(client.getInputStream());
-        output = new PrintWriter(
-        client.getOutputStream(), true
-        );
-      }
-      catch(IOException ioEx) {
-        ioEx.printStackTrace();
-      }
 
       try {
-        if(client.input == "get bid") {
+        if(input.readLine() == "get bid") {
           // echo the current bid
         }
-        if(Integer.parseInt(client.input) > 0) {
+        if(Integer.parseInt(input.readLine()) > 0) {
           if(itemHandler.getCurrentBid() > currentBid){
               currentBid = itemHandler.getCurrentBid();
               //  echo to the user the change in the bid
+              output.println(currentBid);
           } else {
-            // echo bid not set, too low
-            // echo the current bid
+            // echo bid not set, too low & echo current bid
+            output.println("\nYour bid is too low, the current bid is: " + currentBid + "\n");
           }
         } else {
           // Throw error to the user, input not a number
+          output.println("\nPlease input a number \n");
         }
       } catch() {
           // throw invalid input error
+          output.println("\nInvalid input please see the original instructions below  \n");
+          output.println("\nTo place a bid simply enter a number greater than the minimum bid, to find out what the current bid is enter 'get bid' to quit type QUIT\n\n")
       }
 
     } while (!recieved.equals("QUIT"));
